42729de77e9b67154c99f14f52a16584195ca9e5
Gurumbumbum <150255737+Gurumbumbum@users.noreply.github.com>
2026-04-19 21:54:31 +0000

    Update generate_calendar.py

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,20 +1,27 @@
+import requests
+import csv
 from datetime import datetime
 
-fixtures = [
-    ("2026-04-10 20:15", "Vikingur vs Breidablik", "Víkingsvöllur, Reykjavík"),
-    ("2026-04-17 18:30", "KA Akureyri vs Vikingur", "Akureyri"),
-]
+CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTKGm68TxaqhiVGe9Q37iDbt6sbPbltuwJlDcKziPNt0Ut4dBnlcn_DauwnlzmjkmGF_JwSf1PF_k5X/pub?gid=0&single=true&output=csv"
+
+data = requests.get(CSV_URL).text
+rows = csv.reader(data.splitlines())
+
+next(rows)  # skip header
 
 lines = ["BEGIN:VCALENDAR", "VERSION:2.0"]
 
-for i, (dt, title, loc) in enumerate(fixtures):
-    dt = datetime.strptime(dt, "%Y-%m-%d %H:%M").strftime("%Y%m%dT%H%M%S")
+for i, row in enumerate(rows):
+    date, home, away, location, competition = row
+
+    dt = datetime.strptime(date, "%Y-%m-%d %H:%M").strftime("%Y%m%dT%H%M%S")
 
     lines.append(f"""BEGIN:VEVENT
 UID:vikingur-{i}
 DTSTART:{dt}
-SUMMARY:{title}
-LOCATION:{loc}
+SUMMARY:{home} vs {away}
+LOCATION:{location}
+DESCRIPTION:{competition}
 END:VEVENT""")
 
 lines.append("END:VCALENDAR")
